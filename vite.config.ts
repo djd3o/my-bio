b3df56fb353aeb01b075d4b861ec1da4c019e538
Amadeo <its.amad3o@gmail.com>
2026-05-10 20:34:35 -0500

    Adding reqs to publish github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/my-bio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

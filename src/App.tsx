758a07b70aaf584f4b2051a0152dd40b664f314d
Amadeo <its.amad3o@gmail.com>
2026-05-10 20:42:36 -0500

    fixing reference for npm build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "./components/ui/card";
 import { links } from "./data/links";
 import profileImage from "./assets/f-3-bandcamp.jpg";
 

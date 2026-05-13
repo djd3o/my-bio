e819bf505cc37c6142a1349ee384255c769197d2
Amadeo <its.amad3o@gmail.com>
2026-05-12 19:27:12 -0500

    added myt remix

--- src/data/recentLinks.ts
+++ src/data/recentLinks.ts
@@ -3,4 +3,8 @@ export const recentLinks = [
     label: "UKF Premiere",
     url: "https://www.youtube.com/watch?v=lYsNE_DjmmQ&list=RDlYsNE_DjmmQ&start_radio=1",
   },
+  {
+    label: "MYT - This Could Be The End (djD3o edit)",
+    url: "https://soundcloud.com/djd3o/myt-this-could-be-the-end",
+  },
 ];

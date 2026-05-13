b0af26bb20fa9bb22465991619286d1aa81086ee
Amadeo <its.amad3o@gmail.com>
2026-05-12 19:33:12 -0500

    build for github pages

--- src/data/recentLinks.ts
+++ src/data/recentLinks.ts
@@ -4,7 +4,7 @@ export const recentLinks = [
     url: "https://www.youtube.com/watch?v=lYsNE_DjmmQ&list=RDlYsNE_DjmmQ&start_radio=1",
   },
   {
-    label: "MYT - This Could Be The End (djD3o edit)",
+    label: "This Could Be The End (djD3o edit)",
     url: "https://soundcloud.com/djd3o/myt-this-could-be-the-end",
   },
 ];
